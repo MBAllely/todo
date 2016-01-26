636f701fc39eadc15da2c40dbf0d196c8b80b04a
Marika Allely and Tal Haberer <student@epicodus.com>
2016-01-25 16:23:06 -0800

    Add deadline to Tasker

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,36 @@
-function Tasker(newTask) { // Creates prototype/template for new tasks
-  this.newTask = newTask
-}
+function Tasker(task, newDeadline) { // Creates prototype/template for new tasks
+  this.task = task;
+  this.newDeadline = newDeadline;
+};
+
+Tasker.prototype.taskName = function() { //defines a method to be called on newTask
+  return this.task;
+};
 
 $(document).ready(function() {
   $("form#new-task").submit(function(event) {
     event.preventDefault();
 
-    var newTask = $("input#new-task-name").val(); //defines variable from input
+    var task = $("input#new-task-name").val(); //defines variable from input
+    var newDeadline = $("input#new-deadline").val();
+    var newTask = new Tasker(task, newDeadline); //creates new task with given parameters
+
 
     //appends new task to list
-    $("ul#tasks").append("<li><span class='tasks'>" + newTask + "<a href='#' class='close' aria-hidden='true'>&times;</a>" + "</span></li>");
+    $("ul#tasks").append("<li><span class='tasks'>" + newTask.taskName() + "<a href='#' class='close' aria-hidden='true'>&times;</a>" + "</span></li>");
 
     $("input#new-task-name").val("");
+    $("input#new-deadline").val("");
 
     $(".tasks").last().click(function() { //on click, list of tasks shows
-      ("#show-task").show();
-      (".task-name").text(newTask.task-name);
+      $("#show-task").show();
+      $("#show-task h2").text(newTask.taskName());
+      $(".task").text(newTask.task);
+      $(".deadline").text(newTask.newDeadline);
+
     });
     $("body").on('click', '#tasks a', function() {
-       $(this).closest("li").remove();
-  })
-});
+      $(this).closest("li").remove();
+    });
+  });
 });
--- specs/specs.js
+++ specs/specs.js
@@ -1,6 +1,12 @@
 describe ('tasker', function() {
-  it("adds a task to the list of tasks", function(){
-    var testTask = new Tasker("take out the trash");
-    expect(testTask.newTask).to.equal("take out the trash");
+  it("adds a task with the given properties", function(){
+    var testTask = new Tasker("take out the trash", "1.30.16");
+    expect(testTask.task).to.equal("take out the trash");
+    expect(testTask.newDeadline).to.equal("1.30.16");
+  });
+
+  it("adds newTask method to the task; displays a clickable toggle", function() {
+    var testTask = new Tasker("Garbage");
+    expect(testTask.newTask()).to.equal("Garbage");
   });
 });
